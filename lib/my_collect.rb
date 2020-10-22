fd9ae7f44227155b7ee3d28ff9c579d53b6cb06a
hansenjl <jenn.leigh.hansen@gmail.com>
2020-10-22 08:55:39 -0700

    commit tree bcfad8a56756829810e7a58fe24ba3db5e0fd11e

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+require 'pry'
+#returns a modified array 
+def my_collect(array)
+    counter = 0 
+    new_array = [] 
+    while counter < array.length
+       new_value = yield(array[counter])
+       new_array.push(new_value)
+        counter += 1 
+    end
 
+    #return value will be different
+    new_array
+end
 
