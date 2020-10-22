40ee48591c355b7d5278c726ad5cd6cf8e4d2457
hansenjl <jenn.leigh.hansen@gmail.com>
2020-10-22 09:00:19 -0700

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+require 'pry'
+#returns a modified array 
+def my_collect(array)
+    counter = 0 
+    new_array = [] 
+    while counter < array.length
+       new_value = yield(array[counter])
+       new_array.push(new_value)
+        counter += 1 
+    end
+    #return value will be different
+    new_array
+end
+
+empty_array = []
+array.each do |num|
+    empty_array.push(num*num)
+end
+empty_array 
+
+
+array.map do |num|
+    num*num
+end
 
 
